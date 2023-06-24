3b7183af8bb19e736e15a857dff424c8d43838d8
Eric Amorde <eric.amorde@gmail.com>
2023-06-24 13:33:45 -0700

    WIP webhook payload

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 mkdir -p _specs/_site || true
 
 # generate sharded index
-bundle exec ruby Scripts/create_pods_and_versions_index.rb 
+bundle exec ruby Scripts/create_pods_and_versions_index.rb "$@"
 
 cp _specs/*.yml _specs/_site/
 cp index.html _specs/_site
